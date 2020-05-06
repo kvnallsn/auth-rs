e76ccc2ef1782ed0fce07aa6dd0de3c6d70b6561
Kevin Allison <kvnallsn@gmail.com>
2020-05-06 07:47:32 -0400

    rocket: make optional

--- src/password.rs
+++ src/password.rs
@@ -1,56 +1,149 @@
 //! Password based authentication using argon2
 
-use argon2::{self, Config, Error};
+use argon2::{self, Config};
 use rand::RngCore;
-use rocket::{
-    config::Value,
-    fairing::{Fairing, Info, Kind},
-    Rocket,
-};
 
-const ARGON2_TABLE: &str = "argon2";
-const ARGON2_LANES: &str = "lanes";
+// Re-export error type for use downstream
+pub use argon2::{Error, Variant};
 
-pub struct PasswordConfig;
+pub enum Hasher {
+    Argon2(Config<'static>),
+}
 
-#[rocket::async_trait]
-impl Fairing for PasswordConfig {
-    fn info(&self) -> Info {
-        Info {
-            name: "Auth-rs Password Config",
-            kind: Kind::Attach,
+impl Hasher {
+    pub fn new(lanes: u32, memory: u32, passes: u32, variant: Variant) -> Self {
+        let mut argon = Config::default();
+        argon.lanes = lanes;
+        argon.mem_cost = memory;
+        argon.time_cost = passes;
+        argon.variant = variant;
+        Hasher::Argon2(argon)
+    }
+
+    pub fn hash<S: AsRef<str>>(&self, password: S) -> Result<String, Error> {
+        match self {
+            Hasher::Argon2(cfg) => {
+                // use a 16-byte salt
+                let mut salt = [0u8; 16];
+                rand::thread_rng().fill_bytes(&mut salt);
+
+                argon2::hash_encoded(password.as_ref().as_bytes(), &salt, cfg)
+            }
         }
     }
 
-    fn on_attach(&self, rocket: Rocket) -> Result<Rocket, Rocket> {
-        let argon = Config::default();
-        let cfg = rocket.config();
-
-        if let Ok(table) = cfg.get_table(ARGON2_TABLE) {
-            //
-            println!("{:#?}", table);
+    pub fn verify<S, H>(&self, password: S, hash: H) -> Result<bool, Error>
+    where
+        S: AsRef<str>,
+        H: AsRef<str>,
+    {
+        match self {
+            Hasher::Argon2(_) => {
+                argon2::verify_encoded(hash.as_ref(), password.as_ref().as_bytes())
+            }
         }
-
-        Ok(rocket)
     }
 }
 
-pub fn fairing() -> PasswordConfig {
-    PasswordConfig
-}
+#[cfg(feature = "web-rocket")]
+pub mod rocket {
+    use super::Hasher;
+    use argon2::{Config, Variant};
+    use rocket::{
+        fairing::{Fairing, Info, Kind},
+        Rocket,
+    };
 
-pub fn hash<S: AsRef<str>>(password: S, config: &Config) -> Result<String, Error> {
-    // use a 16-byte salt
-    let mut salt = [0u8; 16];
-    rand::thread_rng().fill_bytes(&mut salt);
+    const ARGON2_TABLE: &str = "argon2";
 
-    argon2::hash_encoded(password.as_ref().as_bytes(), &salt, config)
-}
+    /// Number of lanes (i.e., threads) to use
+    const ARGON2_LANES: &str = "lanes";
 
-pub fn verify<S, H>(password: S, hash: H) -> Result<bool, Error>
-where
-    S: AsRef<str>,
-    H: AsRef<str>,
-{
-    argon2::verify_encoded(hash.as_ref(), password.as_ref().as_bytes())
+    /// Amount of memory used by Argon2, in Kibibytes
+    const ARGON2_MEMORY: &str = "memory";
+
+    /// Number of passes (i.e., iterations) over the memory
+    const ARGON2_PASSES: &str = "passes";
+
+    /// Variant of Argon2 to use (argon2i argon2d, argon2id)
+    const ARGON2_VARIANT: &str = "variant";
+
+    macro_rules! get_int {
+        ($field:expr, $table:expr,$rocket:expr) => {
+            match $table.get($field) {
+                Some(v) => match v {
+                    rocket::config::Value::Integer(i) => Some(*i),
+                    _ => {
+                        log::error!("{}: expected an integer, got something else", $field);
+                        return Err($rocket);
+                    }
+                },
+                None => None,
+            }
+        };
+    }
+
+    macro_rules! get_str {
+        ($field:expr, $table:expr,$rocket:expr) => {
+            match $table.get($field) {
+                Some(v) => match v {
+                    rocket::config::Value::String(s) => Some(s.as_ref()),
+                    _ => {
+                        log::error!("{}: expected a string, got something else", $field);
+                        return Err($rocket);
+                    }
+                },
+                None => None,
+            }
+        };
+    }
+
+    pub struct PasswordFairing;
+
+    #[rocket::async_trait]
+    impl Fairing for PasswordFairing {
+        fn info(&self) -> Info {
+            Info {
+                name: "Auth-rs Password Config",
+                kind: Kind::Attach,
+            }
+        }
+
+        fn on_attach(&self, rocket: Rocket) -> Result<Rocket, Rocket> {
+            let mut argon = Config::default();
+            let cfg = rocket.config();
+
+            if let Ok(table) = cfg.get_table(ARGON2_TABLE) {
+                if let Some(lanes) = get_int!(ARGON2_LANES, table, rocket) {
+                    argon.lanes = lanes as u32;
+                }
+
+                if let Some(memory) = get_int!(ARGON2_MEMORY, table, rocket) {
+                    argon.mem_cost = memory as u32;
+                }
+
+                if let Some(passes) = get_int!(ARGON2_PASSES, table, rocket) {
+                    argon.time_cost = passes as u32;
+                }
+
+                if let Some(variant) = get_str!(ARGON2_VARIANT, table, rocket) {
+                    argon.variant = match variant {
+                        "argon2i" => Variant::Argon2i,
+                        "argon2d" => Variant::Argon2d,
+                        "argon2id" => Variant::Argon2id,
+                        x => {
+                            log::error!("argon2: unrecognized variant `{}`", x);
+                            return Err(rocket);
+                        }
+                    }
+                }
+            }
+
+            Ok(rocket.manage(Hasher::Argon2(argon)))
+        }
+    }
+
+    pub fn fairing() -> PasswordFairing {
+        PasswordFairing
+    }
 }
