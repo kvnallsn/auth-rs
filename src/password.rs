28174a0f4d6cf34389a665bd9542a491a11a8bce
Kevin Allison <kvnallsn@gmail.com>
2020-05-07 13:47:13 -0400

    added: custom error type for password hashing
    Added new HasherError to correspond to errors during the hashing
    process, to include invalid/incorrect passwords.  Instead of returng
    bool for success/failure on verification, instead VerificationFailed is
    now included in the Error struct and an Ok value is only return if the
    supplied password matches the hash.

--- src/password.rs
+++ src/password.rs
@@ -2,9 +2,20 @@
 
 use argon2::{self, Config};
 use rand::RngCore;
+use std::default::Default;
+use thiserror::Error;
 
 // Re-export error type for use downstream
-pub use argon2::{Error, Variant};
+pub use argon2::Variant;
+
+#[derive(Error, Debug)]
+pub enum HasherError {
+    #[error("password validation failed")]
+    ValidationFailed,
+
+    #[error("argon2 backend failure: {0}")]
+    Argon2(#[from] argon2::Error),
+}
 
 pub enum Hasher {
     Argon2(Config<'static>),
@@ -20,28 +31,40 @@ impl Hasher {
         Hasher::Argon2(argon)
     }
 
-    pub fn hash<S: AsRef<str>>(&self, password: S) -> Result<String, Error> {
+    pub fn hash<S: AsRef<str>>(&self, password: S) -> Result<String, HasherError> {
         match self {
             Hasher::Argon2(cfg) => {
                 // use a 16-byte salt
                 let mut salt = [0u8; 16];
                 rand::thread_rng().fill_bytes(&mut salt);
 
-                argon2::hash_encoded(password.as_ref().as_bytes(), &salt, cfg)
+                let hashed = argon2::hash_encoded(password.as_ref().as_bytes(), &salt, cfg)?;
+                Ok(hashed)
             }
         }
     }
 
-    pub fn verify<S, H>(&self, password: S, hash: H) -> Result<bool, Error>
+    pub fn verify<S, H>(&self, password: S, hash: H) -> Result<(), HasherError>
     where
         S: AsRef<str>,
         H: AsRef<str>,
     {
         match self {
             Hasher::Argon2(_) => {
-                argon2::verify_encoded(hash.as_ref(), password.as_ref().as_bytes())
+                let result = argon2::verify_encoded(hash.as_ref(), password.as_ref().as_bytes())?;
+                if result {
+                    Ok(())
+                } else {
+                    Err(HasherError::ValidationFailed)
+                }
             }
         }
+    }
+}
+
+impl Default for Hasher {
+    fn default() -> Self {
+        Hasher::Argon2(Config::default())
     }
 }
 
